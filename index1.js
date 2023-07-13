b553682a08045b0aab3bac63ed4fb16568907f28
nadi4567 <yunadisoe8@gmail.com>
2023-07-13 16:12:48 +0630

    adding sillyname modules

--- index1.js
+++ index1.js
@@ -1,13 +1,14 @@
 import express from "express";
 import superheroes from "superheroes";
-
+import generateName from "sillyname";
 const app = express();
 const port = 3000;
 
 
 app.get("/", (req, res) => {
   let name = superheroes.random();
-  res.send(`<h1>Behold, I am ${name}!</h1>`);
+  let sillyName = generateName();
+  res.send(`<h1>Behold, I am ${name}! and My sillyname is ${sillyName}</h1>`);
 });
 
 app.listen(port, () => {
